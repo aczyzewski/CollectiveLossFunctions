35ddd9443c25a9631a368d07d28a6ad0d9ce8468
Adam Czyżewski <adam.czyzewski@student.put.poznan.pl>
2020-01-05 19:28:47 +0100

    Clean up.

--- run_experiment.py
+++ run_experiment.py
@@ -1,8 +1,8 @@
 import argparse
 from typing import Any
 
-from data.downloaders.uci import UCIDatabase
-from data.genreators.synthetic import SyntheticDataGenerator
+from src.data.uci import UCIDatabase
+from src.data.generators import SyntheticDataGenerator
 
 def parse_args() -> Any:
     """ Reads parameters from the command line """
@@ -11,11 +11,13 @@ def parse_args() -> Any:
     parser.add_argument('-n', '--name', type=str, help='Name of the experiment.')
     parser.add_argument('-d', '--datasets', type=str, nargs='+', help='The list of datasets that will be used in the experiment.')
     parser.add_argument('-df', '--datasetsfile', type=str, help='Path to file contains a list of datasets that will be used in the experiment.')
-    parser.add_arguemnt('-c', '--config', type=str, help='Path to file that defines all parameters of the experiment.')
+    parser.add_argument('-c', '--config', type=str, help='Path to file that defines all parameters of the experiment.')
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    print(args)
+    
+    datasets = UCIDatabase()
+    s_generator = SyntheticDataGenerator()
     
 
