88ce104e6c4952d6895db702ebcfd3d069411df2
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-04-02 13:36:45 +0200

    Moving entropy() to src.functional and updating unit tests

--- src/functional.py
+++ src/functional.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 
+from torch import Tensor
+
 def mish(input):
     '''
     Applies the mish function element-wise:
@@ -8,3 +10,21 @@ def mish(input):
     See additional documentation for mish class.
     '''
     return input * torch.tanh(F.softplus(input))
+
+
+def entropy(values: Tensor) -> Tensor:
+    """ Calculates entropy independently for each vector in a tensor
+        Returns results as 1-D tensor """
+    _entropy = lambda vector: -1. * torch.sum(torch.Tensor(
+        [probability * torch.log2(probability) for probability in vector]))
+
+    output_vector = []
+
+    for vector in values:
+        _, counts = torch.unique(vector, return_counts=True)
+        probablity_vector = counts * 1. / torch.sum(counts)
+        output_vector.append(_entropy(probablity_vector))
+
+    return torch.Tensor(output_vector).reshape(-1, 1)
+
+
--- src/losses.py
+++ src/losses.py
@@ -2,6 +2,8 @@ import torch
 from torch import Tensor, FloatTensor
 from typing import Callable
 
+from .functional import entropy
+
 
 def MSE() -> Callable[[Tensor, Tensor], Tensor]:
     def mse(input: Tensor, target: Tensor) -> float:
@@ -13,22 +15,6 @@ def HingeLoss() -> Callable[[Tensor, Tensor], Tensor]:
     def hingeloss(input: Tensor, target: Tensor) -> float:
         return torch.sum(torch.max(torch.Tensor([0]), 0.5 - input * target) ** 2)
     return hingeloss
-
-
-def entropy(values: Tensor) -> Tensor:
-    """ Calculates entropy independently for each vector in a tensor
-        Returns results as 1-D tensor """
-    _entropy = lambda vector: -1. * torch.sum(torch.Tensor(
-        [probability * torch.log2(probability) for probability in vector]))
-
-    output_vector = []
-
-    for vector in values:
-        _, counts = torch.unique(vector, return_counts=True)
-        probablity_vector = counts * 1. / torch.sum(counts)
-        output_vector.append(_entropy(probablity_vector))
-
-    return torch.Tensor(output_vector).reshape(-1, 1)
 
 
 def KNNHingeLoss(train_x: Tensor, train_y: Tensor, k: int = 3, alpha: float = 0.3) -> Callable[[Tensor, Tensor], Tensor]:
