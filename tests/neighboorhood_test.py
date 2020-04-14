d0b08f44a28a39820cd580a1d415eb393e33ff3b
Adam Czyżewski <adam.czyzewski@student.put.poznan.pl>
2020-04-14 13:32:17 +0200

    Add an abstact class as an interface for any KNN method.

---
+++ tests/neighboorhood_test.py
@@ -0,0 +1,15 @@
+import unittest
+import numpy as np
+import torch
+
+from src.neighboorhood import FaissKNN
+
+
+class TestNeighboorhood(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    unittest.main()
