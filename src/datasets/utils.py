e40c8af6bf614b5b348db6f2739740e1a35c4355
Adam Czyżewski <adam.czyzewski@student.put.poznan.pl>
2020-05-04 15:23:45 +0200

    Update basic experiment definition. Fix minor issues.

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -39,14 +39,14 @@ def get_datasets(category: str) -> List[str]:
     return DATASETS[category]
 
 
-def load_dataset(name: str, preprocesing: Pipeline = None
+def load_dataset(name: str, preprocesing: Pipeline = None, **kwargs
                  ) -> Tuple[np.array, np.array]:
     """ Loads dataset from a disk """
     x, y = DATABASE.get_by_name(name).load()
     if preprocesing is None:
         print('Warning: preprocessing method wasn\'t defined! Loading RAW data.')
     else:
-        x, y = preprocesing(x, y)
+        x, y = preprocesing(x, y, **kwargs)
     return x, y
 
 
@@ -55,3 +55,14 @@ def convert_to_dataloader(x: np.array, y: np.array, **kwargs) -> List[str]:
     idx, x, y = Tensor(np.arange(x.shape[0])), Tensor(x), Tensor(y)
     dataset = TensorDataset(idx, x, y)
     return DataLoader(dataset, **kwargs)
+
+
+def simplify_dataset_name(value: str) -> str:
+
+    translation = {
+        ord(' '): ord('_'),
+        ord('('): None,
+        ord(')'): None
+    }
+
+    return value.translate(translation).lower()
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,36 @@
-from typing import List, Dict, Tuple, Callable, Union, Any
+from typing import List, Tuple, Callable, Union, Any
+from functools import reduce
+from collections import Counter
 
+import numpy as np
+from sklearn.model_selection import train_test_split
 from torch import nn, Tensor
-import matplotlib.pyplot as plt
 
 from .mish import Mish
 
 
-def plot_values(values: Dict[str, List[float]], xlabel: str = 'Epoch',
-                ylabel: str = 'Loss', size: Tuple[int, int] = (12, 6)):
-    """ Plots multiple lines on the same plot """
+def split_data(x: np.array, y: np.array, test_size: float = 0.2,
+               val_size: float = 0.2, random_state: int = None
+               ) -> List[Tuple[np.array]]:
+    """ Splits data into train/val/test subsets of a given size """
 
-    plt.rcParams.update({'font.size': 14})
-    plt.figure(figsize=(size))
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.grid(True, linestyle="--")
+    train_x, test_x, train_y, test_y = \
+        train_test_split(x, y, test_size=test_size, random_state=random_state)
 
-    for title, items in values.items():
-        plt.plot(items, label=title)
+    train_x, val_x, train_y, val_y = \
+        train_test_split(train_x, train_y, test_size=val_size, random_state=random_state)
 
-    plt.legend()
-    plt.show()
+    return (train_x, train_y), (val_x, val_y), (test_x, test_y)
 
 
 def get_reduction_method(reduction_type: str
                          ) -> Callable[[Tensor], Union[Tensor, float]]:
     """ Reduces a tensor according to reduction type. This function should be
         probably replaced by some internal torch function written in C
-        (See: https://github.com/pytorch/pytorch/blob/master/torch/nn/_reduction.py) """
+
+        See:
+        https://github.com/pytorch/pytorch/blob/master/torch/nn/_reduction.py
+    """
 
     reduction_methods = {
         'mean': lambda x: x.mean(),
@@ -97,3 +100,48 @@ def get_activation_by_name(name: str) -> Any:
                         or it is not supported.")
 
     return methods[name]
+
+
+def convert_logtis_to_class_distribution(a: Tensor, no_classes: int) -> Tensor:
+    """ Converts each of of a given tensor to probablility class distribution
+
+        Example:
+            >>> no_classes = 4
+            >>> a = [[0, 1, 2, 3], [1, 1, 2, 2], [0, 0, 0, 3]]
+            >>> convert_tensor_to_class_distribution(a, no_classes)
+            [[0.25, 0.25, 0.25, 0.25],
+             [0.,   0.75, 0.25, 0.  ],
+             [0.75, 0.    0.    0.25]]
+
+    """
+
+    # FIXME: Slow!
+    output = []
+    for row in a.numpy():
+        counts = Counter(row.astype('int'))
+        output.append([counts[idx] for idx in range(no_classes)])
+
+    return Tensor(output) / a.shape[1]
+
+
+def iterparams(params):
+    """ Iterate over all possible combination of given parameters """
+
+    keys = list(params.keys())
+    counters = [len(param) for param in params.values()]
+    indices = [0] * len(keys)
+    num_loops = reduce(lambda x, y: x * y, counters)
+
+    for _ in range(num_loops):
+        for c_idx in range(len(indices) - 1, 0, -1):
+            if indices[c_idx] == len(params[keys[c_idx]]):
+                indices[c_idx] = 0
+                indices[c_idx - 1] += 1
+
+        attributes = []
+        for key, value, c_idx in zip(keys, params.values(), indices):
+            attributes.append((key, value[c_idx]))
+        yield dict(attributes)
+
+        indices[-1] += 1
+
